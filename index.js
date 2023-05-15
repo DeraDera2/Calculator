6bfaf27b5457bb8d32178c10add5383215c518eb
Naomi <102546711+DeraDera2@users.noreply.github.com>
2023-05-15 10:13:26 -0700

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+let result = document.getElementById('result');
+let expression = '';
+
+function appendValue(value){
+    expression += value;
+    result.value = expression;
+}
+
+function appendOperator(opeartor){
+    expression += opeartor;
+    result.value = expression;
+}
+
+function calculate(){
+    try{
+        const calculated = eval(expression);
+        result.value = calculated;
+        expression = '';
+    }
+   catch(error){
+    result.value = 'Error';
+}
+}
+
+function clearResult(){
+    expression = '';
+    result.value = '';
+}
